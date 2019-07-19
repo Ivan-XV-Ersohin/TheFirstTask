9d44f1907bc5efe142bff41c99144229a759badf
Ivan Eroshin <ibs.eroshin.ivan@yandex.ru>
2019-07-19 14:05:50 +0300

    Полная версия приложения

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,39 @@
+package application;
+
+import building.Building;
+import houses.Castle;
+import houses.Skyscapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application{
+    public static void main(String []args){
+        Building castle = new Castle(1200, "stone");
+        Building skyscapper = new Skyscapper("wood", 5543);
+        castle.setMaterial("steel");
+        skyscapper.setMaterial("bricks");
+        castle.printMaterial();
+        skyscapper.printMaterial();
+        List<Building> buildingsList = new ArrayList<Building>();
+        buildingsList.add(castle);
+        buildingsList.add(skyscapper);
+        for(Building curBuilding: buildingsList){
+            curBuilding.show();
+            curBuilding.show(5);
+        }
+    }
+}
+
+
+
+
+
+
+
+/*
+* List<T> l = new ArrayList<T>();
+* Building b = new Castle(l);
+*
+*
+ */
